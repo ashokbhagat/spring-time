1415ae2ac36dd808456411d7cfc64236cdb56dbb
Ashok Bhagat <ashoka.bhagat@gmail.com>
2024-11-16 23:23:18 +0530

    spring apps

---
+++ spring-data-demo/src/main/java/com/ashok/spring_prac/spring_data_demo/SpringDataDemoApplication.java
@@ -0,0 +1,13 @@
+package com.ashok.spring_prac.spring_data_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataDemoApplication.class, args);
+	}
+
+}
